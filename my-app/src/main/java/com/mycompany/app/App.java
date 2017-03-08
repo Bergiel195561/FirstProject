8bf00556f134dba66f9e3fd8723576a30fe84c42
Krystian Szabat <flasher49@gmail.com>
2017-03-08 07:37:50 +0100

    Add printMaster function

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Siema!" );
+        printMaster();
+    }
+    
+    private static void printMaster(){
+    	System.out.println("Szabat jest debeœciak");
     }
 }
